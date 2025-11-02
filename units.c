e69e516703b5f876aea02f79eb93a667895c6cc3
SHARON <miyogesharon@gmail.com>
2025-11-02 09:57:14 +0300

    Add files via upload

---
+++ units.c
@@ -0,0 +1,20 @@
+/*
+//display units for year 1 semester 1
+//units for year 1 semester 1
+Name: Miyoge Sharon Nyatichi
+Reg no:PA106/G/28785/25
+Description : program to display units for year 1 semester 1
+*/
+
+#include<stdio.h>
+
+int main() {
+    printf("mathematics for science \n");
+    printf("computer systems and organisation \n");
+    printf("fundamentals of ICT \n");
+    printf("communication skills \n");
+    printf("fundamentals of system analysis \n");
+    printf("introduction to programming and algorithm \n");
+    printf("discrete mathematics \n");
+    return 0;
+}
